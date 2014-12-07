3fb4ca70213991d69b1c3031bee7896fbc6940e9
Ed Copony <ecopony@yahoo.com>
2014-12-06 20:42:12 -0800

    Rolling through all the pitches.

--- pggameday.go
+++ pggameday.go
@@ -1,11 +1,23 @@
 package pggameday
 
 import (
+	"github.com/ecopony/gamedayapi"
 	"log"
 )
 
 // ImportPitchesForTeamAndYears saves all pitch data fields for a team and season.
 func ImportPitchesForTeamAndYears(teamCode string, years []int) {
 	log.Println("Importing for " + teamCode)
+	gamedayapi.FetchByTeamAndYears(teamCode, years, importAllPitches)
+}
 
+func importAllPitches(game *gamedayapi.Game) {
+	log.Println(">>>> " + game.ID + " <<<<")
+	for _, inning := range game.AllInnings().Innings {
+		for _, atBat := range inning.AtBats() {
+			for _, pitch := range atBat.Pitches {
+				log.Println("> " + pitch.Des)
+			}
+		}
+	}
 }
