c0a9d9f97713c945d36142a894789e27a37867a2
Ed Copony <ecopony@yahoo.com>
2014-12-15 21:04:40 -0800

    Refactoring the command line bits to use branching using a map to simplify command validation and break out the command implementations into their own functions.

--- pg/pgmlbgd.go
+++ pg/pgmlbgd.go
@@ -9,63 +9,22 @@ import (
 	"strconv"
 )
 
-var commands = []string{
-	"import-pitches-for-team-and-year",
-	"import-pitches-for-year",
+var commands = map[string]func([]string) {
+	"import-pitches-for-year": importPitchesForYear,
+	"import-pitches-for-team-and-year": importPitchesForTeamAndYear,
 }
 
 func main() {
 	args := os.Args[1:]
 	command := args[0]
-	if !isCommandValid(command) {
+
+	if function, ok := commands[command]; ok {
+		function(args)
+	} else {
 		fmt.Println(fmt.Sprintf("%s is not a valid command. Valid commands:", command))
 		printValidCommands()
 		os.Exit(1)
 	}
-
-	if command == "import-pitches-for-team-and-year" {
-		validateArgLength(args, 2)
-		teamCode := args[1]
-
-		yearArgs := args[2:]
-		var years []int
-		for i := 0; i < len(yearArgs); i++ {
-			year, err := strconv.Atoi(yearArgs[i])
-			if err != nil {
-				fmt.Println("Year is not valid")
-			}
-			years = append(years, year)
-		}
-
-		pggameday.ImportPitchesForTeamAndYears(teamCode, years)
-	} else {	// import-pitches-for-year
-		validateArgLength(args, 1)
-		yearArg := args[1]
-		year, err := strconv.Atoi(yearArg)
-		if err != nil {
-			fmt.Println("Year is not valid")
-		}
-		years := []int{year}
-		teams := gamedayapi.TeamsForYear(year)
-		var wg sync.WaitGroup
-		for _, team := range teams {
-			wg.Add(1)
-			go func(team string) {
-				defer wg.Done()
-				pggameday.ImportPitchesForTeamAndYears(team, years)
-			}(team)
-		}
-		wg.Wait()
-	}
-}
-
-func isCommandValid(command string) bool {
-	for _, validCommand := range commands {
-		if command == validCommand {
-			return true
-		}
-	}
-	return false
 }
 
 func printUsage() {
@@ -75,7 +34,7 @@ func printUsage() {
 func printValidCommands() {
 	printUsage()
 	fmt.Println("Valid commands:")
-	for _, validCommand := range commands {
+	for validCommand, _ := range commands {
 		fmt.Println(fmt.Sprintf("\t%s", validCommand))
 	}
 }
@@ -85,5 +44,41 @@ func validateArgLength(args []string, validLength int) {
 		printUsage()
 		os.Exit(1)
 	}
+}
+
+func importPitchesForYear(args []string) {
+	validateArgLength(args, 1)
+	yearArg := args[1]
+	year, err := strconv.Atoi(yearArg)
+	if err != nil {
+		fmt.Println("Year is not valid")
+	}
+	years := []int{year}
+	teams := gamedayapi.TeamsForYear(year)
+	var wg sync.WaitGroup
+	for _, team := range teams {
+		wg.Add(1)
+		go func(team string) {
+			defer wg.Done()
+			pggameday.ImportPitchesForTeamAndYears(team, years)
+		}(team)
+	}
+	wg.Wait()
+}
+
+func importPitchesForTeamAndYear(args []string) {
+	validateArgLength(args, 2)
+	teamCode := args[1]
+	yearArgs := args[2:]
+	var years []int
+	for i := 0; i < len(yearArgs); i++ {
+		year, err := strconv.Atoi(yearArgs[i])
+		if err != nil {
+			fmt.Println("Year is not valid")
+		}
+		years = append(years, year)
+	}
+
+	pggameday.ImportPitchesForTeamAndYears(teamCode, years)
 }
 
